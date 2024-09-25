9bb180518ace8e74ea38245083103de41520e1d5
Selfvsself <killerman323@gmail.com>
2024-09-25 22:13:45 +0300

    initial commit

---
+++ src/main/java/ru/selfvsself/home_texttotext_api/HomeTextToTextApiApplication.java
@@ -0,0 +1,13 @@
+package ru.selfvsself.home_texttotext_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeTextToTextApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeTextToTextApiApplication.class, args);
+	}
+
+}
